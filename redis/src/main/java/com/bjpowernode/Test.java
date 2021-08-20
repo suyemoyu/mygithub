ae50a93da5be41e746b2aefdb6219a0efc641ed7
gaojianguo <jianguo1212@163.com>
2021-08-20 16:21:49 +0800

    test3

--- redis/src/main/java/com/bjpowernode/Test.java
+++ redis/src/main/java/com/bjpowernode/Test.java
@@ -1,7 +1,21 @@
 package com.bjpowernode;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+        list.add("e");
 
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 }
